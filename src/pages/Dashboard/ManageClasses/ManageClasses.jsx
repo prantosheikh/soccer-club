10e4947d593f53a8d161ee43719b1ec9f1f801af
Pranto Sheikh <prantosheikh834@gmail.com>
2023-06-10 03:34:33 +0600

    manage class code formate ok

--- src/pages/Dashboard/ManageClasses/ManageClasses.jsx
+++ src/pages/Dashboard/ManageClasses/ManageClasses.jsx
@@ -5,23 +5,19 @@ import Swal from "sweetalert2";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageClasses = () => {
-//  const [instructorsClass, setinstructorsClass] = useState([]);
+  //  const [instructorsClass, setinstructorsClass] = useState([]);
   const [axiosSecure] = useAxiosSecure();
   const [feedbackId, setFeedbackId] = useState([]);
 
-  
-
- const { data: instructorsClass = [], refetch } = useQuery(
-   ["instructorsclass"],
-   async () => {
-     const res = await axiosSecure.get("/instructorsclass");
-     return res.data;
-   }
- );
-
+  const { data: instructorsClass = [], refetch } = useQuery(
+    ["instructorsclass"],
+    async () => {
+      const res = await axiosSecure.get("/instructorsclass");
+      return res.data;
+    }
+  );
 
   useEffect(() => {
-   
     // axiosSecure.get("/instructorsclass").then((res) => {
     //   // setinstructorsClass(res?.data);
     //   // console.log(res.data);
@@ -40,46 +36,43 @@ const ManageClasses = () => {
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
       confirmButtonText: "Yes, delete it!",
-    })
-    .then((result) => {
+    }).then((result) => {
       if (result.isConfirmed) {
-    axiosSecure.delete(`/handleDelete/${classes}`).then((res) => {
+        axiosSecure.delete(`/handleDelete/${classes}`).then((res) => {
+          console.log(res);
+          if (res.data.deletedCount > 0) {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Delete Class",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+          refetch();
+        });
+      }
+    });
+  };
+  const handleDenied = (classes) => {
+    setFeedbackId(classes);
+    axiosSecure.patch(`/handleDenied/${classes}`).then((res) => {
       console.log(res);
-      if (res.data.deletedCount > 0) {
+      if (res.data.modifiedCount > 0) {
         Swal.fire({
           position: "top-end",
           icon: "success",
-          title: "Delete Class",
+          title: "Denied Class",
           showConfirmButton: false,
           timer: 1500,
         });
       }
       refetch();
     });
-    
-  }
-})
-     
-  };
-  const handleDenied = (classes) => {
-    setFeedbackId(classes);
-     axiosSecure.patch(`/handleDenied/${classes}`).then((res) => {
-       console.log(res);
-       if (res.data.modifiedCount > 0) {
-         Swal.fire({
-           position: "top-end",
-           icon: "success",
-           title: "Denied Class",
-           showConfirmButton: false,
-           timer: 1500,
-         });
-       }
-       refetch();
-     });
   };
   const handleApproved = (classes) => {
     console.log(classes);
-    
+
     axiosSecure.patch(`/instructorsclass/${classes}`).then((res) => {
       console.log(res);
       if (res.data.modifiedCount > 0) {
@@ -91,134 +84,129 @@ const ManageClasses = () => {
           timer: 1500,
         });
       }
-      refetch()
+      refetch();
     });
   };
 
   const handleFeedback = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const form = event.target;
-    const feedback = form.feedback.value
-    axiosSecure.patch(`/deniedFeedback/${feedbackId}`, { feedback })
-      .then(res => {
+    const feedback = form.feedback.value;
+    axiosSecure
+      .patch(`/deniedFeedback/${feedbackId}`, { feedback })
+      .then((res) => {
         console.log(res);
-         if (res.status === 200) {
-           Swal.fire({
-             position: "top-end",
-             icon: "success",
-             title: "Feedback Send",
-             showConfirmButton: false,
-             timer: 1500,
-           });
-         }
-    })
+        if (res.status === 200) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Feedback Send",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
 
-  }
-
-
-
- 
-
-    return (
-      <div className="overflow-x-auto w-full px-5">
-        <h2 className="text-4xl font-mono mb-12 text-orange-500">
-          Manage <span className="text-blue-500">Classes</span>
-        </h2>
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th> </th>
-              <th>#</th>
-              <th>Image</th>
-              <th>Class name</th>
-              <th> Instructor name</th>
-              <th>Instructor email</th>
-              <th>Available seats</th>
-              <th>Price</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {instructorsClass.map((classes, Index) => (
-              <tr key={classes?._id}>
-                <td>
-                  <FaRegTrashAlt
-                    onClick={() => handleDelete(classes?._id)}
-                    className="text-red-400 text-xl cursor-pointer hover:text-red-700"
-                  ></FaRegTrashAlt>
-                </td>
-                <th>{Index + 1}</th>
-                <td>
-                  <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle w-12 h-12">
-                        <img
-                          src={classes?.newCalss?.ClassImage}
-                          alt="Avatar Tailwind CSS Component"
-                        />
-                      </div>
+  return (
+    <div className="overflow-x-auto w-full px-5">
+      <h2 className="text-4xl font-mono mb-12 text-orange-500">
+        Manage <span className="text-blue-500">Classes</span>
+      </h2>
+      <table className="table">
+     
+        <thead>
+          <tr>
+            <th> </th>
+            <th>#</th>
+            <th>Image</th>
+            <th>Class name</th>
+            <th> Instructor name</th>
+            <th>Instructor email</th>
+            <th>Available seats</th>
+            <th>Price</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {instructorsClass.map((classes, Index) => (
+            <tr key={classes?._id}>
+              <td>
+                <FaRegTrashAlt
+                  onClick={() => handleDelete(classes?._id)}
+                  className="text-red-400 text-xl cursor-pointer hover:text-red-700"
+                ></FaRegTrashAlt>
+              </td>
+              <th>{Index + 1}</th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={classes?.newCalss?.ClassImage}
+                        alt="Avatar Tailwind CSS Component"
+                      />
                     </div>
                   </div>
-                </td>
-                <td>{classes?.newCalss?.ClassName}</td>
-                <td>{classes?.newCalss?.InstructorName}</td>
-                <td>{classes?.newCalss?.InstructorEmail}</td>
-                <td>{classes?.newCalss?.AvailableSeats}</td>
-                <td>{classes?.newCalss?.price}</td>
-                <td>
-                  <button
-                    onClick={() => handleApproved(classes?._id)}
-                    className="btn btn-xs bg-blue-600 me-2 text-white"
-                    disabled={classes?.deniedStatus === "Denied"}
-                  >
-                    approved
-                  </button>
-                  <button
-                    onClick={() => handlePending(classes)}
-                    className="btn btn-xs bg-green-600 me-1 text-white"
-                    disabled={
-                      classes?.newStatus === "approved" ||
-                      classes?.deniedStatus === "Denied"
-                    }
-                  >
-                    Pending
-                  </button>
-                  <button
-                    onClick={() =>
-                      handleDenied(classes?._id) ||
-                      window.my_modal_2.showModal()
-                    }
-                    className="btn btn-xs bg-orange-600 mt-3 text-white"
-                    disabled={classes?.newStatus === "approved"}
-                  >
-                    denied
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <dialog id="my_modal_2" className="modal">
-          <form onSubmit={handleFeedback} method="dialog" className="modal-box">
-            <h3 className="font-bold text-lg mb-4">Feedback Class</h3>
-            <textarea
-              className="textarea w-full h-36 textarea-primary"
-              placeholder="Bio"
-              name="feedback"
-            ></textarea>
-            <input
-              className="btn btn-sm mt-4 btn-outline btn-primary"
-              type="submit"
-              value="Submit"
-            />
-          </form>
-          <form method="dialog" className="modal-backdrop">
-            <button>close</button>
-          </form>
-        </dialog>
-      </div>
-    );
+                </div>
+              </td>
+              <td>{classes?.newCalss?.ClassName}</td>
+              <td>{classes?.newCalss?.InstructorName}</td>
+              <td>{classes?.newCalss?.InstructorEmail}</td>
+              <td>{classes?.newCalss?.AvailableSeats}</td>
+              <td>{classes?.newCalss?.price}</td>
+              <td>
+                <button
+                  onClick={() => handleApproved(classes?._id)}
+                  className="btn btn-xs bg-blue-600 me-2 text-white"
+                  disabled={classes?.deniedStatus === "Denied"}
+                >
+                  approved
+                </button>
+                <button
+                  onClick={() => handlePending(classes)}
+                  className="btn btn-xs bg-green-600 me-1 text-white"
+                  disabled={
+                    classes?.newStatus === "approved" ||
+                    classes?.deniedStatus === "Denied"
+                  }
+                >
+                  Pending
+                </button>
+                <button
+                  onClick={() =>
+                    handleDenied(classes?._id) || window.my_modal_2.showModal()
+                  }
+                  className="btn btn-xs bg-orange-600 mt-3 text-white"
+                  disabled={classes?.newStatus === "approved"}
+                >
+                  denied
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <dialog id="my_modal_2" className="modal">
+        <form onSubmit={handleFeedback} method="dialog" className="modal-box">
+          <h3 className="font-bold text-lg mb-4">Feedback Class</h3>
+          <textarea
+            className="textarea w-full h-36 textarea-primary"
+            placeholder="Bio"
+            name="feedback"
+          ></textarea>
+          <input
+            className="btn btn-sm mt-4 btn-outline btn-primary"
+            type="submit"
+            value="Submit"
+          />
+        </form>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </div>
+  );
 };
 
 export default ManageClasses;
