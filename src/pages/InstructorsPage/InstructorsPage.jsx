85595003cf2c871c0e6a221c626a5ee6eae278a3
Pranto Sheikh <prantosheikh834@gmail.com>
2023-06-12 14:52:28 +0600

    remove instructor page table add card

--- src/pages/InstructorsPage/InstructorsPage.jsx
+++ src/pages/InstructorsPage/InstructorsPage.jsx
@@ -2,16 +2,17 @@ import { useEffect, useState } from "react";
 
 const InstructorsPage = () => {
   const [instructors, setInstructor] = useState();
-  console.log(instructors);
+  // console.log(instructors);
 
  useEffect(() => {
    fetch("http://localhost:3000/instructors")
      .then((res) => res.json())
      .then((data) => {
+      //  console.log(data);
        setInstructor(
          data.filter((instructor) => instructor.role === "instructor")
        );
-       console.log(instructors);
+      //  console.log(instructors);
      });
  }, []); 
 
@@ -19,49 +20,32 @@ const InstructorsPage = () => {
 
 
     return (
-      <div className="mb-80 mt-20 px-10">
+      <>
         <h2 className="text-center text-4xl mb-7 ms-7">Instructor Page</h2>
-        <div className="overflow-x-auto">
-          <tbody>
-            <table className="table">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Photo</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                </tr>
-              </thead>
-
-              {instructors &&
-                instructors.map((instructor, index) => (
-                  <tr key={instructor._id}>
-                    {index + 1}
-                    <td>
-                      <div className="flex items-center space-x-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-12 h-12">
-                            <img
-                              src={instructor?.photo}
-                              alt="Avatar Tailwind CSS Component"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td>{instructor?.name}</td>
-                    <td>{instructor?.email}</td>
-
-                    <th>
-                      <button className="btn btn-ghost btn-xs">details</button>
-                    </th>
-                  </tr>
-                ))}
-            </table>
-          </tbody>
+        <div className=" mt-20  grid md:grid-cols-3 gap-6 mx-8">
+          {instructors &&
+            instructors.map((instructor, index) => (
+              <div key={index} className="card w-96 bg-base-100 shadow-xl">
+                <figure>
+                  <img src={instructor?.photo} alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    <p>{instructor?.name}</p>
+                    <div className="badge badge-secondary">NEW</div>
+                  </h2>
+                  <p>
+                    <p>{instructor?.email}</p>
+                  </p>
+                  <div className="card-actions justify-end">
+                    <div className="badge badge-outline">instructor</div>
+                    <div className="badge badge-outline">intelligent</div>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
-      </div>
+      </>
     );
 };
 
